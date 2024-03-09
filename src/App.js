a4ab62b9e23b13adc1b5d0e88f03e8ba3ce7cec0
sudo <t.takeru0331@icloud.com>
2024-03-10 01:41:08 +0900

    💄 要素の追加

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
+import { useState } from 'react';
 import './App.css';
+import TodoList from './components/TodoList';
+
 
 function App() {
+  const [todos, setTodos] = useState(
+    [{id: 1, name: "todo1", completed: false}]
+  );
+
   return (
-    <div className="App"></div>
+    <div className="App">
+      <TodoList todos={todos}/>
+      <input type="text" />
+      <button>Add Task</button>
+      <button>Clear Task</button>
+      <div>Remaining tasks: 0</div>
+    </div>
   );
 }
 
